9991eb4039572eade9df28bb099628b26c1df717
My name <iakylbek005@gmail.com>
2021-01-06 15:40:09 +0600

    Added newfile.

--- main/settings.py
+++ main/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
@@ -116,3 +116,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 2000
+REDIS_DB = 1
